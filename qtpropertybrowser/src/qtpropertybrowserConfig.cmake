01a9eaab7941d80a1ce0c1694eeb068eff5429f9
Dylan Bespalko <dylan.bespalko@gmail.com>
2019-07-27 11:56:36 -0700

    Fixes to CMake ability to find internal packages that are already installed.

--- qtpropertybrowser/src/qtpropertybrowserConfig.cmake
+++ qtpropertybrowser/src/qtpropertybrowserConfig.cmake
@@ -1,3 +1,4 @@
+get_filename_component(QTPROPERTYBROWSER_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 if(NOT TARGET qtpropertybrowser::qtpropertybrowser)
-    include(${CMAKE_PREFIX_PATH}/qtpropertybrowser/qtpropertybrowserTargets.cmake)
+    include(${QTPROPERTYBROWSER_CMAKE_DIR}/qtpropertybrowserTargets.cmake)
 endif()
